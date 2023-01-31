1100ff6344db541903ea0571e20c6c40b3995142
Ben Whitfield-Heap <benjaminpheap@gmail.com>
2023-01-31 12:38:03 +0000

    Updates
    Added more calculations and separated out results

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-let netSalary = 100000
-const workingDays = 260
-let daySalary = 0
-let holidayDaysSalary = 0
-// const daySalary = netSalary / workingDays
-// let holidayDays = 25
-// const holidayDaysSalary = holidayDays * daySalary
-let paternityDays = 0
-const paternityDaysSalary = paternityDays * daySalary
-
 const calculateBtn = document.getElementById('calculate-btn')
-const packageValue = document.getElementById('package-value')
-const netSalaryInput = document.getElementById('net-salary-input')
+const grossSalaryInput = document.getElementById('gross-salary-input')
+const grossSalaryValue = document.getElementById('gross-salary-value')
+const weeklyHoursInput = document.getElementById('weekly-hours-input')
+const hourlyWageValue = document.getElementById('hourly-wage-value')
+const holidaysInput = document.getElementById('holidays-input')
+const holidaysValue = document.getElementById('holidays-value')
+
+const workingDays = 260
+
 
 
 function holidayPay() {
-    daySalary = netSalary / workingDays
+    daySalary = grossSalary / workingDays
     let holidayDays = document.getElementById('holidays').value
     holidayDaysSalary = holidayDays * daySalary
     
@@ -23,25 +20,36 @@ function holidayPay() {
 const formatter = new Intl.NumberFormat('en-EN', {
     style: 'currency',
     currency: 'GBP',
-  
-    // These options are needed to round to whole numbers if that's what you want.
-    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
   });
 
 
 
 calculateBtn.addEventListener('click', function() {
-    //get salary input and update netsalary
-    netSalary = Number(netSalaryInput.value)
-
-    //get holidays and update holidat salary
-    holidayPay()
-    let totalPackageSum = netSalary + holidayDaysSalary
-    packageValue.innerHTML = formatter.format(totalPackageSum)
+    renderGrossSalary()
+    renderHourlyWage()
+    renderHolidayValue()
 })
 
+function renderGrossSalary() {
+    grossSalaryValue.innerText = formatter.format(grossSalaryInput.value)
+}
 
+function renderHourlyWage() {
+    const weeklyWage = grossSalaryInput.value / 52
+    const hourlyWage = weeklyWage / weeklyHoursInput.value
+    hourlyWageValue.innerText = formatter.format(hourlyWage)
+}
+
+function renderHolidayValue() {
+    const dailyWage = grossSalaryInput.value / workingDays
+    const holidayWage = dailyWage *  holidaysInput.value
+    holidaysValue.innerText = formatter.format(holidayWage)
+
+}
+
+function renderPaternityValue() {
+
+}
 
 
 
