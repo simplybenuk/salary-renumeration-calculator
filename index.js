1bf1138acd0c37db6a157115f4e219462aa28bdd
Ben Whitfield-Heap <benjaminpheap@gmail.com>
2023-10-29 10:40:25 +0000

    Removed 4 day week calc

--- index.js
+++ index.js
@@ -5,50 +5,18 @@ function calculateEffectiveSalary() {
   const employerPension1 = parseFloat(document.getElementById('employerPension1').value);
   const annualLeave1 = parseFloat(document.getElementById('annualLeave1').value);
   const parentalLeave1 = parseFloat(document.getElementById('parentalLeave1').value);
-  const fourDayWeek1 = document.getElementById('fourDayWeek1').checked;
+  // const fourDayWeek1 = document.getElementById('fourDayWeek1').checked;
 
   // Get values for Job 2
   const basicSalary2 = parseFloat(document.getElementById('basicSalary2').value);
   const employerPension2 = parseFloat(document.getElementById('employerPension2').value);
   const annualLeave2 = parseFloat(document.getElementById('annualLeave2').value);
   const parentalLeave2 = parseFloat(document.getElementById('parentalLeave2').value);
-  const fourDayWeek2 = document.getElementById('fourDayWeek2').checked;
+  // const fourDayWeek2 = document.getElementById('fourDayWeek2').checked;
 
-  // Calculate the value of a 4-day week (assuming 260 workdays a year)
-  const fourDayWeekValue1 = fourDayWeek1 ? (basicSalary1 / 260) * 52 : 0;
-  const fourDayWeekValue2 = fourDayWeek2 ? (basicSalary2 / 260) * 52 : 0;
-
-  // Calculate pension contributions for both jobs
-  const pensionContribution1 = basicSalary1 * (employerPension1 / 100);
-  const pensionContribution2 = basicSalary2 * (employerPension2 / 100);
-
-  // Calculate annual leave value for both jobs (assuming 260 workdays a year)
-  const annualLeaveValue1 = (basicSalary1 / 260) * annualLeave1;
-  const annualLeaveValue2 = (basicSalary2 / 260) * annualLeave2;
-
-  // Calculate parental leave value for both jobs (assuming 52 workweeks a year)
-  const parentalLeaveValue1 = (basicSalary1 / 52) * parentalLeave1;
-  const parentalLeaveValue2 = (basicSalary2 / 52) * parentalLeave2;
-
-// Function to calculate the effective salary and other benefits
-function calculateEffectiveSalary() {
-  // Get values for Job 1
-  const basicSalary1 = parseFloat(document.getElementById('basicSalary1').value);
-  const employerPension1 = parseFloat(document.getElementById('employerPension1').value);
-  const annualLeave1 = parseFloat(document.getElementById('annualLeave1').value);
-  const parentalLeave1 = parseFloat(document.getElementById('parentalLeave1').value);
-  const fourDayWeek1 = document.getElementById('fourDayWeek1').checked;
-
-  // Get values for Job 2
-  const basicSalary2 = parseFloat(document.getElementById('basicSalary2').value);
-  const employerPension2 = parseFloat(document.getElementById('employerPension2').value);
-  const annualLeave2 = parseFloat(document.getElementById('annualLeave2').value);
-  const parentalLeave2 = parseFloat(document.getElementById('parentalLeave2').value);
-  const fourDayWeek2 = document.getElementById('fourDayWeek2').checked;
-
-  // Calculate the value of a 4-day week (assuming 260 workdays a year)
-  const fourDayWeekValue1 = fourDayWeek1 ? (basicSalary1 / 260) * 52 : 0;
-  const fourDayWeekValue2 = fourDayWeek2 ? (basicSalary2 / 260) * 52 : 0;
+  // // Calculate the value of a 4-day week (assuming 260 workdays a year)
+  // const fourDayWeekValue1 = fourDayWeek1 ? (basicSalary1 / 260) * 52 : 0;
+  // const fourDayWeekValue2 = fourDayWeek2 ? (basicSalary2 / 260) * 52 : 0;
 
   // Calculate pension contributions for both jobs
   const pensionContribution1 = basicSalary1 * (employerPension1 / 100);
@@ -81,7 +49,6 @@ function calculateEffectiveSalary() {
         <h3>Other Benefits</h3>
         <ul>
           <li>Parental Leave Value for Job 1 (in days): £${Number(parentalLeaveValue1.toFixed(2)).toLocaleString()}</li>
-          <li>4-Day Week Value for Job 1: £${Number(fourDayWeekValue1.toFixed(2)).toLocaleString()}</li>
         </ul>
       </section>
       <section class="govuk-section">
@@ -94,7 +61,6 @@ function calculateEffectiveSalary() {
         <h3>Other Benefits</h3>
         <ul>
           <li>Parental Leave Value for Job 2 (in days): £${Number(parentalLeaveValue2.toFixed(2)).toLocaleString()}</li>
-          <li>4-Day Week Value for Job 2: £${Number(fourDayWeekValue2.toFixed(2)).toLocaleString()}</li>
         </ul>
       </section>
     </div>
